51ebcc20e7bdfa4b8f67bc1bd9b827a244cbac0c
Chaitanya Sharma <chaitanya.sharma@mail.vinove.com>
2021-03-25 14:45:02 +0530

    SplashScreen

--- App.js
+++ App.js
@@ -4,11 +4,16 @@ import Navigation from './src/navigations';
 import {Provider} from 'react-redux';
 import store from './src/redux/reducers';
 import {NavigationContainer} from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+  }
+
+  componentDidMount() {
+    SplashScreen.hide();
   }
 
   render() {
--- android/app/src/main/java/com/grainz/MainActivity.java
+++ android/app/src/main/java/com/grainz/MainActivity.java
@@ -1,6 +1,8 @@
 package com.grainz;
 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen; // Import this.
+import android.os.Bundle; // Import this.
 
 public class MainActivity extends ReactActivity {
 
@@ -8,8 +10,18 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+
+  // Add this method.
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);
+      super.onCreate(savedInstanceState);
+  }
+
   @Override
   protected String getMainComponentName() {
     return "Grainz";
   }
+  
+
 }
